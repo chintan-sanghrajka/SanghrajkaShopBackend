914de85e8390527ff592c870e261eba248079d3e
ChintanKS <chintansanghrajka2509@gmail.com>
2023-09-03 23:22:24 +0530

    Hosting

--- index.js
+++ index.js
@@ -47,3 +47,20 @@ app.use(wishListRouter);
 app.use(userRouter);
 
 app.use(subCategoryRouter);
+
+// Your code
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "build", "index.html"),
+      function (err) {
+        if (err) {
+          res.status(500).send(err);
+        }
+      }
+    );
+  });
+}
+// Your code
